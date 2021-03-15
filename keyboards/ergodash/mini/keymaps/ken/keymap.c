366e4bab147fea856e7e995328c881a234b20f25
Ken <veryshj123@gmail.com>
2021-03-15 22:25:14 +0900

    Update keymap.c

--- keyboards/ergodash/mini/keymaps/ken/keymap.c
+++ keyboards/ergodash/mini/keymaps/ken/keymap.c
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC ,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   _______,                        _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_LANG2,                       KC_LANG1, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT , \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_TAB ,                        KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_L1  , \
-    _______, _______, _______, KC_LCTL,            LOWER,   KC_TAB , _______,      _______, KC_SPC , RAISE,            KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_ESC ,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  _______,                       _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LANG2,                      KC_LANG1, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT , \
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_TAB ,                       KC_SPC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_L1  , \
+    _______, _______, _______, KC_LCTL,            LOWER,  KC_TAB , _______,     _______, KC_SPC , RAISE,            KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Lower
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,----------------------------------------------------------------------------------------------------------------------.
   */
   [_RAISE] = LAYOUT(
-    KC_ESC , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                        _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-    KC_LGUI, _______, _______, _______, _______, _______, _______,                        _______, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV , \
-    KC_LSFT, _______, _______, _______, _______, _______,  KC_TAB,                        KC_SPC , KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, \
-    _______, _______, _______, KC_LCTL,             LOWER,   KC_TAB, _______,    _______, KC_SPC , RAISE,            KC_RALT, _______, _______, _______  \
+    KC_ESC , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,                       _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+    KC_LGUI, _______, _______, _______, _______, _______, _______,                       _______, KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV , \
+    KC_LSFT, _______, _______, _______, _______, _______,  KC_TAB,                       KC_SPC , KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, \
+    _______, _______, _______, KC_LCTL,             LOWER, KC_TAB, _______,     _______, KC_SPC , RAISE,            KC_RALT, _______, _______, _______  \
     ),
 
   /* Adjust
