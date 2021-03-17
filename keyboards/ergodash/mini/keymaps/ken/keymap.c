531a0fe313aaee5a9b5ba9dca9673e94b4cb4a17
Ken <veryshj123@gmail.com>
2021-03-17 11:42:28 +0900

    Update keymap.c

--- keyboards/ergodash/mini/keymaps/ken/keymap.c
+++ keyboards/ergodash/mini/keymaps/ken/keymap.c
@@ -97,19 +97,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ref: https://okapies.hateblo.jp/entry/2019/02/02/133953
 static bool lower_pressed = false;
 static bool raise_pressed = false;
+static uint16_t key_timer = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case LOWER:
       if (record->event.pressed) {
         lower_pressed = true;
+        key_timer = timer_read();
         layer_on(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
           
-        if (lower_pressed) {
+        if (lower_pressed && timer_elapsed(key_timer) < 200) {
+          // fire LANG2
           register_code(KC_LANG2);
           unregister_code(KC_LANG2);
         }
@@ -119,13 +122,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case RAISE:
       if (record->event.pressed) {
         raise_pressed = true;
+        key_timer = timer_read();
         layer_on(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
           
-        if (raise_pressed) {
+        if (raise_pressed && timer_elapsed(key_timer) < 200) {
+          // fire LANG1
           register_code(KC_LANG1);
           unregister_code(KC_LANG1);
         }
