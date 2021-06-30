d42fe1eccc367ae9061baba57b2ae1c12c30ba8b
Ken <veryshj123@gmail.com>
2021-07-01 00:17:19 +0900

    Update keymap.c

--- keyboards/crkbd/keymaps/ken/keymap.c
+++ keyboards/crkbd/keymaps/ken/keymap.c
@@ -171,8 +171,8 @@ void oled_task_user(void) {
 static bool l_pressed = false;
 static bool r_pressed = false;
 static uint16_t key_timer = 0;
-#define KC_L MO(1)
-#define KC_R MO(2)
+#define KC_L KC_LCTL
+#define KC_R KC_RALT
 
 bool fire_lang1_lang2(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
